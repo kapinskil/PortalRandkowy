097f362f8a38b9c405aee2a91dd5cb8522e13f5c
kapinskil <kapinski.l@gmail.com>
2020-06-13 23:51:43 +0200

    Values CRUD

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers
 {
@@ -12,11 +13,60 @@ namespace PortalRandkowy.API.Controllers
     [ApiController]
     //http://localhost:5000/api
     [Route("api/[controller]")]
-    public class ValuesController: ControllerBase
+    public class ValuesController : ControllerBase
     {
-        public ActionResult<string[]> Get()
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
         {
-            return new string[] {"value1","value2"};
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult AddValue([FromBody]Value value)
+        {
+            _context.Values.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditValue(int id,[FromBody]Value value)
+        {
+            var data = _context.Values.Find(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            _context.SaveChanges();
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteValue(int id)
+        {
+            var data = _context.Values.Find(id);
+
+            if(data == null)
+            {
+                return NoContent();
+            }
+
+            _context.Values.Remove(data);
+            _context.SaveChanges();
+            return Ok(data);
         }
     }
 }
