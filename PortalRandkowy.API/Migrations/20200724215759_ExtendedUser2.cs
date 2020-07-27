634a393d2656712d3cf172c53dc392addd3bf1a7
kapinskil <kapinski.l@gmail.com>
2020-07-27 19:50:51 +0200

    update database

---
+++ PortalRandkowy.API/Migrations/20200724215759_ExtendedUser2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortalRandkowy.API.Migrations
+{
+    public partial class ExtendedUser2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateodBirth",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateofBirth",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateofBirth",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateodBirth",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
