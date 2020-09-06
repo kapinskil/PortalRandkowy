032d9a1171b985fd912fd3849e652b3cc4134140
kapinskil <kapinski.l@gmail.com>
2020-09-06 21:23:45 +0200

    Added forwarding after login

--- PortalRandkowy.SPA/src/app/_services/auth.service.ts
+++ PortalRandkowy.SPA/src/app/_services/auth.service.ts
@@ -19,10 +19,9 @@ login(model: any){
   return this.http.post(this.baseUrl + 'login', model)
     .pipe(map((response: any) => {
       const user = response;
-      if(user){
+      if (user){
         localStorage.setItem('token', user.token);
         this.decodeToken = this.jwtHelper.decodeToken(user.token);
-        console.log(this.decodeToken);
       }
     }));
 }
