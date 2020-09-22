70a6ca759cc31b859777c07addeaa065a418a369
kapinskil <kapinski.l@gmail.com>
2020-09-22 22:35:33 +0200

    modification of the template user-edit

--- PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/User';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -10,8 +12,9 @@ import { User } from 'src/app/_models/User';
 export class UserEditComponent implements OnInit {
 
   user: User;
+  @ViewChild('editForm') editForm: NgForm;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data =>{
@@ -19,4 +22,9 @@ export class UserEditComponent implements OnInit {
     });
   }
 
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profil pomyslnie zaktualizowany');
+    this.editForm.reset(this.user);
+  }
 }
